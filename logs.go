39fc350567ba8ea2b7e7033dffc78ebf8fba469c
daqian.zhang <daqian.zhang@g42.ai>
2021-12-06 18:15:23 +0400

    add rolling log

--- logs.go
+++ logs.go
@@ -1,13 +1,12 @@
 package lex
 
 import (
-	"fmt"
 	"io"
 	"os"
-	"path/filepath"
 	"runtime/debug"
 
 	"github.com/rs/zerolog"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func init() {
@@ -17,48 +16,35 @@ func init() {
 	}
 }
 
-const (
-	Method = "method"
-	Module = "module"
-	App    = "app"
-	IP     = "ip"
-)
-
-func NewLog(filename string, writers ...io.Writer) zerolog.Logger {
-	if filename != "" {
-		logFile, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-		if err != nil {
-			fmt.Println(err)
-		}
-		writers = append(writers, logFile)
+// maxSize:unit megabytes, maxAge:unit days
+func NewRollingWriter(fileName string, maxSize, maxAge, maxBackups int, compress, localtime bool) io.Writer {
+	return &lumberjack.Logger{
+		Filename:   fileName,
+		MaxSize:    maxSize, // megabytes
+		MaxBackups: maxBackups,
+		MaxAge:     maxAge,   //days
+		Compress:   compress, // disabled by default
+		LocalTime:  localtime,
 	}
-
-	multi := zerolog.MultiLevelWriter(writers...)
-	log := zerolog.New(multi).With().Timestamp().Logger()
-	return log
 }
 
-func GetLogger(logFile string, level string, withStdout bool) zerolog.Logger {
-	if logFile != "" {
-		if !filepath.IsAbs(logFile) {
-			wd, err := os.Getwd()
-			if err == nil {
-				logFile = filepath.Join(wd, logFile)
-			}
-		}
-		logDir := filepath.Dir(logFile)
-		if _, err := os.Stat(logDir); err != nil {
-			os.MkdirAll(logDir, 0755)
-		}
+func NewFileWriter(fileName string) (io.Writer, error) {
+	return os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+}
+
+func NewStdoutWriter() io.Writer {
+	return zerolog.ConsoleWriter{Out: os.Stdout}
+}
+
+func NewStderrWriter() io.Writer {
+	return zerolog.ConsoleWriter{Out: os.Stderr}
+}
+
+func NewLogger(tags map[string]string, writers ...io.Writer) zerolog.Logger {
+	multi := zerolog.MultiLevelWriter(writers...)
+	ctx := zerolog.New(multi).With()
+	for k, v := range tags {
+		ctx = ctx.Str(k, v)
 	}
-	var writers []io.Writer
-	if withStdout {
-		writers = append(writers, os.Stdout)
-	}
-	r := NewLog(logFile, writers...)
-	lvl, err := zerolog.ParseLevel(level)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return r.Level(lvl)
+	return ctx.Timestamp().Logger()
 }
